2448b252d04d88fddb483ad8bf1d2003cb37f147
arghya <sahaarghya5678@gmail.com>
2023-12-29 22:47:59 +0530

    initial

---
+++ src/hooks/useCurrencyInfo.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import { useState,useEffect } from "react";
+function useCurrencyInfo (currency){
+    const [data,setData]= useState({})
+    useEffect(()=>{
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        .then((res)=>res.json())
+        .then((res)=>setData(res[currency])) //returning the whole parameter of the ["USD"-->key] / currency
+    },[currency])
+    console.log(data);
+    return data;
+}
+export default useCurrencyInfo;
